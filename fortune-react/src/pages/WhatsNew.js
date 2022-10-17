d1a24877fb6dec8495824e821439ca49deafd706
cleog <goldscleo@gmail.com>
2022-10-17 14:03:32 -0700

    Additions to what's new page

--- fortune-react/src/pages/WhatsNew.js
+++ fortune-react/src/pages/WhatsNew.js
@@ -3,12 +3,22 @@ import React from 'react';
 function WhatsNew() {
     return (
         <div>
-            <h3>New Feature</h3>
+            <h2>New Feature:</h2>
             <p>
                 Write your own fortunes to add to the possible options!
             </p>
+            <h3>Benefits:</h3>
             <p>
-                For previous version select "Easy Home". For new version select "Advanced Home"
+                You can be creative, entertain other users, and add great fortunes to the mix!
+            </p>
+            <h3>Costs:</h3>
+            <p>
+                Those who were happy with the previous version or who have lower computer self-efficacy may find this new feature confusing at first.
+            </p>
+            <h2>What's Next?</h2>
+            <p>
+                For previous version select "Easy Home". 
+                For new version select "Advanced Home"
             </p>
         </div>
     )
